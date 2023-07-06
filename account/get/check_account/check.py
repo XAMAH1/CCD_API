e5a69cb5a3ad3be445cdf9aff169527da21fd25f
XAMAH <ilya_Bata@mail.ru>
2023-07-06 19:40:22 +0500

    1

--- account/get/check_account/check.py
+++ account/get/check_account/check.py
@@ -1,21 +1,27 @@
 import random
 from database.base import *
 from account.get.recording.recording import record
+import datetime
 
 def check(connect):
-    all_account = all_account_get(connect)
-    passed_account = passed_account_get(connect)
-    if len(all_account) == len(passed_account):
-        return {"success": True, "message": "account undefined"}
-    job = True
-    while job:
-        current_account = random.choice(all_account)
-        if current_account not in passed_account:
-            record(connect, current_account)
-            return current_account
+    try:
+        all_account = all_account_get(connect)
+        passed_account = passed_account_get(connect)
+        if len(all_account) == len(passed_account):
+            return {"success": True, "message": "account undefined"}
+        job = True
+        while job:
+            current_account = random.choice(all_account)
+            if current_account not in passed_account:
+                record(connect, current_account)
+                return current_account
+    except:
+        return {"success": True, "message": "not account"}
 
 def all_account_get(connect):
-    command = select(account)
+    now = datetime.datetime.now()
+    current_date = now.strftime("%d.%m.%Y")
+    command = select(account).where(account.c.date_parish == current_date)
     result = connect.execute(command)
     current_account = []
     for i in result:
@@ -23,7 +29,9 @@ def all_account_get(connect):
     return current_account
 
 def passed_account_get(connect):
-    command = select(history_account)
+    now = datetime.datetime.now()
+    current_date = now.strftime("%d.%m.%Y")
+    command = select(history_account).where(history_account.c.date_parish == current_date)
     result = connect.execute(command)
     current_account = []
     for i in result:
