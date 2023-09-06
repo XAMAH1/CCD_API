7b740a310ec04accc07d67da0c03012e88559399
XAMAH1 <cuperopen@mail.ru>
2023-09-06 08:30:29 +0300

    1

--- examination/examination/check_time/time_check.py
+++ examination/examination/check_time/time_check.py
@@ -7,7 +7,7 @@ def time_check_error(connect, current_account):
     nowtime = datetime(int(current_account[4].split(".")[2]), int(current_account[4].split(".")[1]), int(current_account[4].split(".")[0]), int(current_account[5].split(":")[0]), int(current_account[5].split(":")[1]), int(current_account[5].split(":")[2]))
     run_at = nowtime + timedelta(minutes=20)
     if now > run_at:
-        command = update(history_account).values({"success": "Ошибка. Время на выаполнение закончилось", "date_expiration": run_at}).where(history_account.c.id == current_account[0])
+        command = update(history_account).values({"success": "Ошибка. Время на выполнение закончилось", "date_expiration": run_at}).where(history_account.c.id == current_account[0])
         connect.execute(command)
         connect.commit()
         #   print(f'{run_at}\t{nowtime}\t UPDATE')
